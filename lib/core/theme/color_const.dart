acdcf3aadc0a78c181dfdf9f7fd2fb9418632375
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-02-23 22:17:01 +0530

    Added onboarding screen

---
+++ lib/core/theme/color_const.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class ColorConst {
+  static const Color kPurple = Color(0xff4A154B);
+}
---
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:slack/core/theme/color_const.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: ColorConst.kPurple,
+    );
+  }
+}
