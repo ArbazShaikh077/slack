6b3a6ed5c93fdc7700e6227e9751a3487d2d15d0
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-02-25 22:23:07 +0530

    Modified onboarding screen

--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -17,9 +17,9 @@ class _OnBoardingState extends State<OnBoarding> {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+          children: [
             SizedBox(
-              height: 80,
+              height: 120,
             ),
             Text(
               "Picture this: a\nmessaging app,",
@@ -35,6 +35,54 @@ class _OnBoardingState extends State<OnBoarding> {
                   fontWeight: FontWeight.bold,
                   fontSize: 30),
             ),
+            SizedBox(
+              height: 400,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Go on...",
+                    style: TextStyle(
+                        color: ColorConst.kPurple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 15)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7))),
+                      overlayColor: MaterialStateProperty.all(Colors.black12),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.white))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 15)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7))),
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.transparent),
+                  overlayColor: MaterialStateProperty.all(Colors.white10),
+                ),
+              ),
+            )
           ],
         ),
       ),
