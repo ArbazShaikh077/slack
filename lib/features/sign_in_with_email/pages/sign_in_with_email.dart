f581df6d76ac1da8b9146db8d70c79ec74d91109
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-03-04 21:52:25 +0530

    Modified sign in with email screen

--- lib/features/sign_in_with_email/pages/sign_in_with_email.dart
+++ lib/features/sign_in_with_email/pages/sign_in_with_email.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:slack/core/theme/color_const.dart';
@@ -30,18 +31,45 @@ class _SignInWithEmailState extends State<SignInWithEmail> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "Your email address",
-              style:
-                  TextStyle(color: Colors.white60, fontWeight: FontWeight.w400),
+              style: TextStyle(
+                  color: Colors.white60,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14.sp),
             ),
             TextField(
-              decoration: InputDecoration(
+              style: TextStyle(fontSize: 18.sp, color: Colors.white),
+              decoration: const InputDecoration(
                 border: InputBorder.none,
                 focusedBorder: InputBorder.none,
                 errorBorder: InputBorder.none,
               ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Text(
+              "We\'ll send you an email that'll instantly sign you in.",
+              style: TextStyle(
+                  color: Colors.white60,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14.sp),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.h),
+              child: const Divider(
+                height: 0,
+                color: Colors.white38,
+              ),
+            ),
+            Text(
+              "Sign in to GovSlack?",
+              style: TextStyle(
+                  color: Colors.white60,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 15.sp),
             )
           ],
         ),
