a240f6e3f73ef2aac30cc7dd7ecea8bc5f0a66a7
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-03-05 22:49:10 +0530

    Added govslack sign in option

--- lib/features/sign_in_with_email/pages/sign_in_with_email.dart
+++ lib/features/sign_in_with_email/pages/sign_in_with_email.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:slack/core/theme/color_const.dart';
@@ -11,6 +10,7 @@ class SignInWithEmail extends StatefulWidget {
 }
 
 class _SignInWithEmailState extends State<SignInWithEmail> {
+  ValueNotifier<bool> signInToSlack = ValueNotifier(true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +19,14 @@ class _SignInWithEmailState extends State<SignInWithEmail> {
         automaticallyImplyLeading: false,
         backgroundColor: ColorConst.kBlack,
         centerTitle: false,
-        title: Text(
-          'Sign in',
-          style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold),
-        ),
+        title: ValueListenableBuilder<bool>(
+            valueListenable: signInToSlack,
+            builder: (_, signInToSlack, __) {
+              return Text(
+                signInToSlack ? 'Sign in' : 'Sign in to GovSlack',
+                style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold),
+              );
+            }),
         elevation: 0.5,
         shadowColor: Colors.white24,
       ),
@@ -32,13 +36,19 @@ class _SignInWithEmailState extends State<SignInWithEmail> {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Your email address",
-              style: TextStyle(
-                  color: Colors.white60,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14.sp),
-            ),
+            ValueListenableBuilder<bool>(
+                valueListenable: signInToSlack,
+                builder: (_, signInToSlack, __) {
+                  return Text(
+                    signInToSlack
+                        ? "Your email address"
+                        : "You email address for GovSlack",
+                    style: TextStyle(
+                        color: Colors.white60,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.sp),
+                  );
+                }),
             TextField(
               style: TextStyle(fontSize: 18.sp, color: Colors.white),
               decoration: const InputDecoration(
@@ -64,16 +74,35 @@ class _SignInWithEmailState extends State<SignInWithEmail> {
                 color: Colors.white38,
               ),
             ),
-            Text(
-              "Sign in to GovSlack?",
-              style: TextStyle(
-                  color: Colors.white60,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 15.sp),
-            )
+            ValueListenableBuilder<bool>(
+                valueListenable: signInToSlack,
+                builder: (_, signInToSlack, __) {
+                  return GestureDetector(
+                    onTap: signInToSlack
+                        ? switchToGovSlackSignIn
+                        : switchToSlackSignIn,
+                    child: Text(
+                      signInToSlack
+                          ? "Sign in to GovSlack?"
+                          : "Sign in to Slack?",
+                      style: TextStyle(
+                          color: Colors.white60,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15.sp),
+                    ),
+                  );
+                })
           ],
         ),
       ),
     );
+  }
+
+  void switchToGovSlackSignIn() {
+    signInToSlack.value = false;
+  }
+
+  void switchToSlackSignIn() {
+    signInToSlack.value = true;
   }
 }
